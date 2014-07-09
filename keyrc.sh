24a421eba83203f31ee43625e4bc84eed33502ca
congto <tcvn1985@gmail.com>
2014-07-09 15:45:04 +0700

    Loại bỏ file thừa
    ok

--- keyrc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-#
-# Tao file chua bien moi truong
-
-touch keyrc
-echo "export OS_USERNAME=admin" >> keyrc
-echo "export OS_PASSWORD=a" >> keyrc
-echo "export OS_TENANT_NAME=admin" >> keyrc
-echo "export OS_AUTH_URL=http://controller:35357/v2.0" >> keyrc
